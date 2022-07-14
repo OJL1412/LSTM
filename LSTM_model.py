85122b5104831038d6d2a904d0081122f4e303db
OJL <1791088334@qq.com>
2022-07-14 10:54:26 +0800

    LSTM封装处理，编写解码部分

--- LSTM_model.py
+++ LSTM_model.py
@@ -2,103 +2,107 @@ import torch
 import torch.nn as nn
 
 
-class M_LSTM(nn.Module):
-    def __init__(self, vocab_size, emb_dim=32, hidden_state=None, cell_state=None, drop_out=0.1, bind_emb=True):
-        super(M_LSTM, self).__init__()
+class LSTMCell_X(nn.Module):
+    """
+    x: 决定使用多少层LSTM
+    i_size: 前馈神经网络的输入大小
+    o_size: 即隐状态
+    激活函数前加nn.LayerNorm: 先算o_size个数的均值和方差，再计算（o_size-均值）/方差，用以排除极大和极小的数，稳定数据
+    激活函数后接nn.Dropout: 用来随机丢掉部分数据特征（弄成0），使得剩下的数据特征包含丢掉的数据的特征，使数据的特征间产生关联
+    """
 
-        self.v_size = vocab_size
+    def __init__(self, x, i_size, o_size=None, dropout=0.1):
+        super(LSTMCell_X, self).__init__()
 
-        hidden_state = emb_dim if hidden_state is None else hidden_state  # 给定一个默认的输出值
-        cell_state = emb_dim if cell_state is None else cell_state  # 给定一个默认的输出值
+        o_size = i_size if o_size is None else o_size
 
-        # Embedding进行词嵌入，随机初始化映射为一个向量矩阵，参数1是嵌入字典的词的数量，参数2是每个嵌入向量的大小，此处为词向量维度32
-        self.w_emb = nn.Embedding(self.v_size, emb_dim)
+        self.trans = nn.Linear(i_size + o_size, o_size * x, bias=False)
+        self.norm = nn.LayerNorm([x, o_size])
+        self.act = nn.GELU()
+        self.drop = nn.Dropout(dropout)
 
-        self.i_gate = nn.Sequential(
-            nn.Linear(emb_dim + hidden_state, hidden_state, bias=False),
-            nn.LayerNorm(hidden_state),
-            nn.Sigmoid(),
-            nn.Dropout(drop_out)
-        )
+    def forward(self, input, state):
+        output, cell = state
+        o_size = list(output.size())
+        o_size.insert(-1, 4)
 
-        self.f_gate = nn.Sequential(
-            nn.Linear(emb_dim + hidden_state, hidden_state, bias=False),
-            nn.LayerNorm(hidden_state),
-            nn.Sigmoid(),
-            nn.Dropout(drop_out)
-        )
+        # 第1步计算: Cell(i-1)和Output(i-1) --> Cell(i)和Output(i)
+        handle = self.norm(self.trans(torch.cat((input, output), dim=-1)).view(o_size))
 
-        self.o_gate = nn.Sequential(
-            nn.Linear(emb_dim + hidden_state, hidden_state, bias=False),
-            nn.LayerNorm(hidden_state),
-            nn.Sigmoid(),
-            nn.Dropout(drop_out)
-        )
+        f_gate, i_gate, o_gate = handle.narrow(-2, 0, 3).sigmoid().unbind(-2)
+        h_state = self.drop(self.act(handle.select(-2, 3)))
 
-        self.act = nn.Sequential(
-            nn.Linear(emb_dim + hidden_state, hidden_state, bias=False),
-            nn.LayerNorm(hidden_state),
-            nn.GELU(),
-            nn.Dropout(drop_out)
-        )
+        # 第2步计算
+        cell = f_gate * cell + i_gate * h_state
+        output = o_gate * cell
 
-        self.classifier = nn.Sequential(
-            nn.Linear(hidden_state, emb_dim, bias=False),
-            nn.Linear(emb_dim, self.v_size)  # 32 --> vocab_size
-        )
+        return output, cell
 
-        self.hidden_state_init = nn.Parameter(torch.zeros(1, hidden_state))  # 隐状态初始化
 
-        if bind_emb:
-            self.classifier[-1].weight = self.w_emb.weight  # 绑定词向量与分类器的权重
+class LSTMLayer(nn.Module):
+    def __init__(self, i_size=32, o_size=None):
+        super(LSTMLayer, self).__init__()
+
+        o_size = i_size if o_size is None else o_size
+
+        self.net = LSTMCell_X(4, i_size, o_size)
+
+        self.init_hx = nn.Parameter(torch.zeros(1, o_size))
+        self.init_cx = nn.Parameter(torch.zeros(1, o_size))
 
     def forward(self, input):
-        output = self.hidden_state_init.expand(input.size(0), -1)
-        cell = self.hidden_state_init.expand(input.size(0), -1)
-        lstm_input = self.w_emb(input)
+        hidden_state = self.init_hx.expand(input.size(0), -1)
+        cell = self.init_cx.expand(input.size(0), -1)
 
         _state = []
-        for i in lstm_input.unbind(1):
-            concat_result = torch.cat((i, output), dim=-1)
 
-            f_gate_state = self.f_gate(concat_result)
-            o_gate_state = self.o_gate(concat_result)
-            i_gate_state = self.i_gate(concat_result)
-            h_state = self.act(concat_result)
+        for i in input.unbind(1):
+            hidden_state, cell = self.LC_net(i, (hidden_state, cell))
+            _state.append(hidden_state)
 
-            product_cell_fgate = cell * f_gate_state
-            product_igate_h = i_gate_state * h_state
-
-            current_cell = product_cell_fgate + product_igate_h
-            current_output = current_cell * o_gate_state
-
-            output = current_output
-            cell = current_cell
-
-            _state.append(output)
-
-        state = self.classifier(torch.stack(_state, dim=1))
-
-        return state
-
-    def decode(self, input):
-        output_state = self.hidden_state_init.expand(input.size(0), -1)
-        cell_state = self.hidden_state_init.expand(input.size(0), -1)
-        lstm_input = self.w_emb(input)
-
-        concat_result = torch.cat((lstm_input, output_state), dim=-1)
-
-        f_gate_state = self.f_gate(concat_result)
-        o_gate_state = self.o_gate(concat_result)
-        i_gate_state = self.i_gate(concat_result)
-        h_state = self.act(concat_result)
-
-        product_cell_fgate = cell_state * f_gate_state
-        product_igate_h = i_gate_state * h_state
-
-        current_cell = product_cell_fgate + product_igate_h
-        current_output = current_cell * o_gate_state
-
-        output = torch.argmax(self.classifier(current_output), dim=1)
+        output = torch.stack(_state, dim=1)
 
         return output
+
+    def decode(self, input, hidden_state=None, cell=None):
+        hidden_state = self.init_hx.expand(input.size(0), -1) if hidden_state is None else hidden_state
+        cell = self.init_cx.expand(input.size(0), -1) if cell is None else cell
+
+        hidden_state, cell = self.net(input, (hidden_state, cell))
+
+        return hidden_state, cell
+
+
+class M_LSTM(nn.Module):
+    def __init__(self, v_size, emb_size=32, h_size=None, bind_emb=True):
+        super(M_LSTM, self).__init__()
+
+        h_size = emb_size if h_size is None else h_size
+
+        # Embedding进行词嵌入，随机初始化映射为一个向量矩阵，参数1是嵌入字典的词的数量，参数2是每个嵌入向量的大小，此处为词向量维度32
+        self.w_emb = nn.Embedding(v_size, emb_size)
+        self.net = LSTMLayer()
+
+        self.classifier = nn.Sequential(
+            nn.Linear(h_size, emb_size, bias=False),
+            nn.Linear(emb_size, v_size)
+        )
+
+        if bind_emb:
+            self.classifier[-1].weight = self.w_emb.weight
+
+    def forward(self, input):
+        lstm_input = self.w_emb(input)
+
+        output = self.classifier(self.net(lstm_input))
+
+        return output
+
+    def decode(self, input, hidden_state, cell):
+        lstm_input = self.w_emb(input)
+
+        hidden_state, cell = self.net.decode(lstm_input, hidden_state, cell)
+
+        output = torch.argmax(self.classifier(hidden_state), dim=1)
+
+        return output, hidden_state, cell

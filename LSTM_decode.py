c75f8df4adf6454903a9ffef1d114a15efff5877
OJL <1791088334@qq.com>
2022-07-18 17:27:30 +0800

    LSTM解码代码更新，添加循环保存每层处理后的(h_state, cell)

--- LSTM_decode.py
+++ LSTM_decode.py
@@ -24,10 +24,10 @@ if __name__ == '__main__':
     # print(sentence)
 
     sentence = replace_word(sentence, en)
-    print(sentence)
+    # print(sentence)
 
     word2id_l = word2id(en2index, sentence)
-    # print(word2id)
+    # print(word2id_l)
 
     model = M_LSTM(len(en2index), num_layers)
     net_state_dict = torch.load(PATH_MODEL, map_location='cpu')
@@ -41,23 +41,31 @@ if __name__ == '__main__':
     model.eval()
 
     decode_step = len(sentence)
-    predict_step = 10
-    h_state = None
-    cell = None
+    predict_step = int(input("请输入要预测的词数:"))
+
+    record = {}
     result = []
 
-    # 先将整句话过一遍decode
+    print("--"*11, "句子的处理", "--"*11)
+
+    # 先将整句话过一遍decode,可以理解为将整句话的每个词的hidden_state全部集中于最后一个词的hidden_state上，进而预测最后一个词的下一个词
     for i in range(decode_step):
         words = torch.LongTensor([word2id_l[i]]).to(device)
-        output, h_state, cell = model.decode(words, h_state, cell)
+        output, record = model.decode(words, record)
+        print("下一个可能的词的tensor为:{}".format(output))
 
+    print("--"*11, "后续词预测", "--"*11)
+
+    # 在经过整句话的decode处理后，从整句话预测的最后的一个词的下一个词开始预测
     for i in range(decode_step-1, predict_step):
         words = torch.LongTensor([word2id_l[i]]).to(device)  # 每次取一个词的索引
-        output, h_state, cell = model.decode(words, h_state, cell)  # 解码获得最可能的下一个词
-        word2id_l.append(output.item())
+        output, record = model.decode(words, record)  # 解码获得最可能的下一个词
+        print("下一个可能的词的tensor为:{}".format(output))
+        word2id_l.append(output.item())  # 将获得的词的索引加入word2id_l继续处理
 
         result.append(output.item())
 
+    print("--"*11, "预测的结果", "--"*11)
     result_words = id2word(en2index, result)
-
+    print("预测的后续{}个词汇为:{}".format(len(result_words), result_words))
     print("预测句子结果为:{}".format(" ".join(result_words).replace("@@ ", "")))
